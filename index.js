fea6b9cff664255b2450f88bbe7f687cf9cad312
Andrew Lincoln <andrewjlincoln@gmail.com>
2017-09-22 22:59:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,20 @@ function nowServing(line) {
     return "There is nobody waiting to be served!"
   }
 }
+
+
+function currentLine(line) {
+  var theLine = `The line is currently: 1. ${theLine[0]}`
+
+  if (line.length === 0) {
+    return "The line is currently empty."
+  }
+
+  else {
+    for (var d = 1; d <= line.length; d++) {
+      theLine = theLine + `, ${d + 1}. ${line[d]}`
+    }
+    return theLine;
+  }
+  
+}
