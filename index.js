a9bbddda8d5c7f0ff67ce8f4d0bab436a84a33dc
Andrew Lincoln <andrewjlincoln@gmail.com>
2017-09-22 23:00:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function nowServing(line) {
 
 
 function currentLine(line) {
-  var theLine = `The line is currently: 1. ${theLine[0]}`
+  var theLine = `The line is currently: 1. ${line[0]}`
 
   if (line.length === 0) {
     return "The line is currently empty."
